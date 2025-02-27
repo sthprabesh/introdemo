a249dcb5d99afdaa5b8d4c15ac941f177c325ff7
sthaprabesh <prabesh.shresthabata@centria.fi>
2025-02-27 14:08:33 +0200

    Second  Commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    extensions: ['.js', '.jsx'], // Make sure both .js and .jsx are resolved
-  },
   plugins: [react()],
-});
+})
